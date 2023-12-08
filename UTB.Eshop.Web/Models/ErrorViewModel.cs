9a0907771ce8b2999912622c5a4c261fa69e2d63
FunTom <vogeltanz@utb.cz>
2023-12-08 09:45:25 +0100

    Přidejte soubory projektu.

---
+++ UTB.Eshop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UTB.Eshop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
